d95e9915ff8bac7f0684e2047e1e1615a82a3e56
kaveeshaPathirathna <as20240416@sci.sjc.ac.lk>
2025-10-25 14:41:02 +0530

    add mainMenu function and cityMangement function

--- main.c
+++ main.c
@@ -1,10 +1,91 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+#define MAX_CITY 30
+#define MAX_DELIVERY 50
+#define MAX_NAME 50
+
+
+
+typedef struct{
+char type[10];
+int capacity;
+float avgSpeed;
+float ratePerKm;
+float fuelEfficiency;
+}vehicle;
+
+
+
+
+char city[MAX_CITY][MAX_NAME];
+int distances[MAX_CITY][MAX_CITY];
+vehicle vehicles[3];
+int cityCount = 0;
+int deliveryCount = 0;
+
+void mainMenu();
+void cityManagement();
+
+
+void addCity ();
+
+
 int main()
 {
-    
+
 }
+void mainMenu()
+{
+    printf("\n=======================================\n");
+    printf("     LOGISTICS MANAGEMENT SYSTEM\n");
+    printf("\n=======================================\n");
+    printf("1. City Management\n");
+
+
+
+}
+
+void cityManagement()
+{
+    int choice;
+
+    do {
+        printf("\n--- CITY MANAGMENT---");
+        printf("1. Add City\n");
+        printf("2. Remove City\n");
+        printf("3. Rename City\n");
+        printf("4. Display City\n");
+        printf("5. back to main \n");
+        printf("Enter your choice : ");
+        scanf("%d",&choice );
+
+        switch(choice)
+    case 1 :
+                break ;
+
+    case 2 :
+                break ;
+
+    case 3 :
+                break ;
+
+    case 4 :
+                break ;
+
+    case 5 :
+                break ;
+
+    default :
+        printf("Invalid choice!\n");
+
+
+    }
+    while (choice !=5);
+}
+
+
 void addCity ()
 {
     if (cityCount >= MAX_CITY)
